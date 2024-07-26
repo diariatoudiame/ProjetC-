cea3977d9412df52058d106be58584759a73ae72
diariatoudiame <diamediariatou344@gmail.com>
2024-07-26 16:18:45 +0000

    changes

---
+++ Pages/Rv.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace application_web.Pages
+{
+    public class RvModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- obj/Debug/net8.0/application_web.AssemblyInfo.cs
+++ obj/Debug/net8.0/application_web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("application_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e7a875331a4a784beea07cb4f8eab99980aa854")]
 [assembly: System.Reflection.AssemblyProductAttribute("application_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("application_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
